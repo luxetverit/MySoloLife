e0644ee64784ae587a8c729a6b2073d402498903
luxetverit <replaysoccer@naver.com>
2021-12-13 17:58:10 +0900

    add http test view & button

--- app/src/main/java/com/example/mysololife/MainActivity.kt
+++ app/src/main/java/com/example/mysololife/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import com.example.mysololife.auth.IntroActivity
+import com.example.mysololife.auth.LoginActivity
+import com.example.mysololife.test.testHttpActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -30,5 +32,12 @@ class MainActivity : AppCompatActivity() {
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
             startActivity(intent)
         }
+
+        findViewById<Button>(R.id.testHttpBtn).setOnClickListener {
+            val intent = Intent(this, testHttpActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }

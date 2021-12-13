42e9c24d73ea7c9a07ee041f5c4034512d57edfe
luxetverit <replaysoccer@naver.com>
2021-12-13 17:45:37 +0900

    add http test view

---
+++ app/src/main/java/com/example/mysololife/test/testHttpActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mysololife.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mysololife.R
+
+class testHttpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_http)
+    }
+}
